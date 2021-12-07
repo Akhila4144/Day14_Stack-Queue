fb225483271a561df9c45c6442ba3b81b028c29b
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 15:30:50 +0530

    By Popping Elements UC2 Done

---
+++ StackUC2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");        
+            Console.WriteLine("The Peek Num is");
+            stack.Peek();
+            Console.WriteLine(stack.Pop());           
+            Console.WriteLine("After pop the peek number the stack will be:");
+            Console.WriteLine("--------");
+            Console.WriteLine("The Peek Num is");
+            stack.Peek();
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine("After pop the peek number the stack will be:");
+            Console.WriteLine("--------");
+            Console.WriteLine("The Peek Num is");
+            stack.Peek();
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine("After pop the peek number the stack will be:");
+            Console.WriteLine("Now The stack is Empty");
+
+
+
+        }
+    }
+}
