7a85bbf84273b53bbf78742e055b4a6895a9b628
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 15:28:30 +0530

    By Creating Stack UC1 Done

---
+++ StackUC_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackUC_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            foreach(int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------");
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
