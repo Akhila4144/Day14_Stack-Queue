9450a30b1306f36dd2a313270851409fd2d12f44
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 15:35:27 +0530

    By removing Elements Using dequeue

---
+++ QueuesUC4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueuesUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------");
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine("--------");
+            queue.Dequeue();
+           Console.WriteLine(queue.Peek());
+            Console.WriteLine("--------");
+             queue.Dequeue();
+           Console.WriteLine(queue.Peek());
+            Console.WriteLine("--------");
+             queue.Dequeue();
+           Console.WriteLine(queue.Peek());
+
+
+
+
+
+
+        }
+    }
+}
