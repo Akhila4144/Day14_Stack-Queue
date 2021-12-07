44d2b169052169cedaf8886df0a633ce2bc48864
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 15:33:10 +0530

    By Creating Queue UC3 DOne

---
+++ QueueUC3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            foreach(int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(queue.Peek());
+
+
+
+        }
+    }
+}
